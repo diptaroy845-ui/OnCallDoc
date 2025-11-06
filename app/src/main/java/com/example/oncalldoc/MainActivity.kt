42ce357af480e6c11f06c87cd22b2ca876322d4d
diptaroy845-ui <diptaroy845@gmail.com>
2025-11-06 15:57:43 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/oncalldoc/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.oncalldoc
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var usernameInput: EditText
+    lateinit var passwordInput: EditText
+    lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginButton = findViewById(R.id.login_btn)
+
+        loginButton.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            Log.i("Test Credentials", "Username = $username, Password = $password")
+        }
+    }
+}
