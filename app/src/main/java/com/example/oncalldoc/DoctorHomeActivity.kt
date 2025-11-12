6be9613e97102f4d80656ff1f8eeaf39b9effb64
talukderalifmahmood <tlkdralif@gmail.com>
2025-11-12 19:20:53 +0600

    Fixed bugs

---
+++ app/src/main/java/com/example/oncalldoc/DoctorHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.oncalldoc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DoctorHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor_home)
+    }
+}
