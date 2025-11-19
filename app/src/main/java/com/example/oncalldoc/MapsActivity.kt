e931bce9663cee404bead9033de66ab946ff38f4
diptaroy845-ui <diptaroy845@gmail.com>
2025-11-20 00:04:21 +0600

    Change in LinearFormat

---
+++ app/src/main/java/com/example/oncalldoc/MapsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.oncalldoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // This activity is intentionally left blank to prevent build errors.
+    }
+}
